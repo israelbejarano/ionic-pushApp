5f8000082323f623a663b4ec65228e2f2f06030c
israel <israel.bejarano.a@gmail.com>
2019-08-28 17:03:50 +0200

    notificaciones push + localStorage

--- src/app/services/push.service.ts
+++ src/app/services/push.service.ts
@@ -1,19 +1,38 @@
-import { Injectable } from '@angular/core';
-import { OneSignal } from '@ionic-native/onesignal/ngx';
+import { Injectable, EventEmitter } from '@angular/core';
+import { OneSignal, OSNotification, OSNotificationPayload } from '@ionic-native/onesignal/ngx';
+import { Storage } from '@ionic/storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PushService {
 
-  constructor(private oneSignal: OneSignal) { }
+  mensajes: OSNotificationPayload[] = [
+    // {
+    //   title: 'Titulo de la push',
+    //   body: 'Este es el body de la push',
+    //   date: new Date()
+    // }
+  ];
+
+  userId: string;
+
+  pushListener = new EventEmitter<OSNotificationPayload>();
+
+  constructor(private oneSignal: OneSignal, private storage: Storage) { }
+
+  async getMensajes() {
+    await this.cargarMensajes();
+    return [...this.mensajes];
+  }
 
   configuracionInicial() {
     this.oneSignal.startInit('a2c12948-7fd7-4a8c-8578-46e85c60ea98', '868116619793');
-    this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
+    this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.Notification);
     this.oneSignal.handleNotificationReceived().subscribe((noti) => {
      // do something when notification is received
      console.log('Notificación recibida', noti);
+     this.notificacionRecibida(noti);
     });
     this.oneSignal.handleNotificationOpened().subscribe((noti) => {
       // do something when a notification is opened
@@ -21,4 +40,47 @@ export class PushService {
     });
     this.oneSignal.endInit();
   }
+
+  async getUserIdOneSignal() {
+    console.log('Cargando userId');
+    // Obtener ID del suscriptor
+    const info = await this.oneSignal.getIds();
+    this.userId = info.userId;
+    return info.userId;
+  }
+
+  async notificacionRecibida(noti: OSNotification) {
+
+    await this.cargarMensajes();
+
+    const payload = noti.payload;
+
+    const existePush = this.mensajes.find(mensaje => mensaje.notificationID === payload.notificationID);
+
+    if (existePush) {
+      return;
+    }
+    this.mensajes.unshift(payload);
+    this.pushListener.emit(payload);
+    await this.guardarMensajes();
+  }
+
+  guardarMensajes() {
+    this.storage.set('mensajes', this.mensajes);
+  }
+
+  async cargarMensajes() {
+
+    this.mensajes =  await this.storage.get('mensajes') || [];
+
+    return this.mensajes;
+
+  }
+
+  async borrarMensajes() {
+    await this.storage.clear();
+    this.mensajes = [];
+    this.guardarMensajes();
+  }
+
 }
